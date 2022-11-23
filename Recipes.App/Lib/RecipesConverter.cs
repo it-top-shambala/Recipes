e71e7630436e014d941f0cecf4f6e2432b04c2ef
Starinin Andrey <starininandrey@gmail.com>
2022-11-23 20:03:48 +0300

    Add RecipesConverter

---
+++ Recipes.App/Lib/RecipesConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Recipes.Models;
+
+using Recipe = Recipes.App.Models.Recipe;
+
+namespace Recipes.App.Lib;
+
+public static class RecipesConverter
+{
+    public static async IAsyncEnumerable<Recipe> ConvertAsync(Root recipes)
+    {
+        foreach (var recipe in recipes.Hits.Select(hit => hit.Recipe))
+        {
+            yield return await Task.Run(() => new Recipe
+            {
+                Title = recipe.Label,
+                Ingredients = new ObservableCollection<string>(recipe.IngredientLines),
+                Image = recipe.Images.THUMBNAIL.Url
+            });
+        }
+    }
+}
